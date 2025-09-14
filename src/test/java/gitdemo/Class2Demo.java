d5ae54618efd6f81d63199360ab4bef96d454450
SudheerKumarBeera-1995 <bsk17987@gmail.com>
2025-09-14 20:18:43 +0530

    Committed new Class2Demo from Demo2

---
+++ src/test/java/gitdemo/Class2Demo.java
@@ -0,0 +1,10 @@
+package gitdemo;
+
+import cucumber.api.cli.Main;
+
+public class Class2Demo {
+	public static void main(String[] args) {
+		System.out.println("Cloned and committed new code");
+	}
+
+}
